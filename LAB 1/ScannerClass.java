7a08a54bc1f387cae7a54a692ddd7ba9ba3a586e
Desai Jineesh <160141047+jineeshdesai09@users.noreply.github.com>
2024-12-17 22:40:14 +0530

    Basic Program

---
+++ LAB 1/ScannerClass.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class ScannerClass {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter 1st number ");
+    int a = sc.nextInt();
+    System.out.print("Enter 2nd number ");
+    int b = sc.nextInt();
+    int sum=a+b;
+    System.out.print("Sum is equls to ");
+        System.out.println(sum);
+    
+    }
+}
